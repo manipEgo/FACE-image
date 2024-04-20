ddb74fbb65a395c957ff8728240e57b1677cbbe0
manip_ego <52987267+manipEgo@users.noreply.github.com>
2024-04-20 14:40:16 +0800

    refactor: remove --group_nodes, always use group nodes

--- src/workflow_generation.py
+++ src/workflow_generation.py
@@ -102,12 +102,6 @@ def parse_args() -> argparse.Namespace:
         help="Upscale height",
     )
     parser.add_argument(
-        "--group_nodes",
-        type=bool,
-        default=True,
-        help="Whether to group nodes or not",
-    )
-    parser.add_argument(
         "--steps",
         type=int,
         default=64,
@@ -121,7 +115,9 @@ def main(args: argparse.Namespace):
     nodes = []
     id_cnt = 0
 
-    # Checkpoint Loader: 0
+    # === Nodes ===
+    # Human images
+    # Estimator Checkpoint Loader: 0
     nodes.append(
         {
             "id": id_cnt,
@@ -135,7 +131,7 @@ def main(args: argparse.Namespace):
         }
     )
     id_cnt += 1
-    # CLIP Encoder: 1, 2
+    # Estimator CLIP Encoder: 1, 2
     nodes.append(
         {
             "id": id_cnt,
@@ -203,112 +199,38 @@ def main(args: argparse.Namespace):
 
         # Mask In-paints
         for j in range(args.mask_cnt):
-            if args.group_nodes:
-                # Mask: 5 + (2 + args.mask_cnt) * i + j
-                nodes.append(
-                    {
-                        "id": id_cnt,
-                        "type": "workflow/mask",
-                        "pos": [1000 + 400 * (j + 1), 700 * i],
-                        "inputs": [
-                            {"name": "pixels", "type": "IMAGE", "link": None},
-                            {"name": "vae", "type": "VAE", "link": None},
-                            {"name": "model", "type": "MODEL", "link": None},
-                            {"name": "positive", "type": "CONDITIONING", "link": None},
-                            {"name": "negative", "type": "CONDITIONING", "link": None},
-                            {"name": "VAEDecode vae", "type": "VAE", "link": None},
-                        ],
-                        "title": f"Image-{i} Mask-{j}",
-                        "widgets_values": [
-                            f"mask_{j}.png",
-                            "alpha",
-                            "image",
-                            0,
-                            0,
-                            "randomize",
-                            args.steps,
-                            0,
-                            "euler",
-                            "normal",
-                            1,
-                            f"image-{i}_mask-{j}",
-                        ],
-                    }
-                )
-                id_cnt += 1
-            else:
-                # VAE Encoder: 5 + (2 + 5 * args.mask_cnt) * i + 5 * j
-                nodes.append(
-                    {
-                        "id": id_cnt,
-                        "type": "VAEEncodeForInpaint",
-                        "inputs": [
-                            {"name": "pixels", "type": "IMAGE", "link": None},
-                            {"name": "vae", "type": "VAE", "link": None},
-                            {"name": "mask", "type": "MASK", "link": None},
-                        ],
-                        "outputs": [
-                            {"name": "LATENT", "type": "LATENT", "links": [], "slot_index": 0}
-                        ],
-                        "widgets_values": [0],
-                    }
-                )
-                id_cnt += 1
-                # Image Mask Loader: 6 + (2 + 5 * args.mask_cnt) * i + 5 * j
-                nodes.append(
-                    {
-                        "id": id_cnt,
-                        "type": "LoadImageMask",
-                        "outputs": [
-                            {"name": "MASK", "type": "MASK", "links": [], "slot_index": 0}
-                        ],
-                        "widgets_values": [f"mask_{j}.png", "alpha", "image"],
-                    }
-                )
-                id_cnt += 1
-                # KSampler: 7 + (2 + 5 * args.mask_cnt) * i + 5 * j
-                nodes.append(
-                    {
-                        "id": id_cnt,
-                        "type": "KSampler",
-                        "inputs": [
-                            {"name": "model", "type": "MODEL", "link": None},
-                            {"name": "positive", "type": "CONDITIONING", "link": None},
-                            {"name": "negative", "type": "CONDITIONING", "link": None},
-                            {"name": "latent_image", "type": "LATENT", "link": None},
-                        ],
-                        "outputs": [
-                            {"name": "LATENT", "type": "LATENT", "links": [], "slot_index": 0}
-                        ],
-                        "widgets_values": [0, "randomize", args.steps, 0, "euler", "normal", 1],
-                    }
-                )
-                id_cnt += 1
-                # VAE Decoder: 8 + (2 + 5 * args.mask_cnt) * i + 5 * j
-                nodes.append(
-                    {
-                        "id": id_cnt,
-                        "type": "VAEDecode",
-                        "inputs": [
-                            {"name": "samples", "type": "LATENT", "link": None},
-                            {"name": "vae", "type": "VAE", "link": None},
-                        ],
-                        "outputs": [
-                            {"name": "IMAGE", "type": "IMAGE", "links": [], "slot_index": 0}
-                        ],
-                    }
-                )
-                id_cnt += 1
-                # Image Saver: 9 + (2 + 5 * args.mask_cnt) * i + 5 * j
-                nodes.append(
-                    {
-                        "id": id_cnt,
-                        "type": "SaveImage",
-                        "inputs": [{"name": "images", "type": "IMAGE", "link": None}],
-                        "widgets_values": [f"image-{i}_mask-{j}"],
-                    }
-                )
-                id_cnt += 1
+            # Mask: 5 + (2 + args.mask_cnt) * i + j
+            nodes.append(
+                {
+                    "id": id_cnt,
+                    "type": "workflow/mask",
+                    "pos": [1000 + 400 * (j + 1), 700 * i],
+                    "inputs": [
+                        {"name": "pixels", "type": "IMAGE", "link": None},
+                        {"name": "vae", "type": "VAE", "link": None},
+                        {"name": "model", "type": "MODEL", "link": None},
+                        {"name": "positive", "type": "CONDITIONING", "link": None},
+                        {"name": "negative", "type": "CONDITIONING", "link": None},
+                        {"name": "VAEDecode vae", "type": "VAE", "link": None},
+                    ],
+                    "title": f"Image-{i} Mask-{j}",
+                    "widgets_values": [
+                        f"mask_{j}.png",
+                        "alpha",
+                        "image",
+                        0,
+                        0,
+                        "randomize",
+                        args.steps,
+                        0,
+                        "euler",
+                        "normal",
+                        1,
+                        f"image-{i}_mask-{j}",
+                    ],
+                }
+            )
+            id_cnt += 1
 
     # === Links ===
     # link id
@@ -329,119 +251,50 @@ def main(args: argparse.Namespace):
     links.append([link_cnt, nodes[0]["id"], 1, nodes[2]["id"], 0])
     link_cnt += 1
 
-    if args.group_nodes:
-        ## Image Loader -> Image Scaler
-        for i in range(args.image_cnt):
-            nodes[3 + (2 + args.mask_cnt) * i]["outputs"][0]["links"].append(link_cnt)
-            nodes[4 + (2 + args.mask_cnt) * i]["inputs"][0].update({"link": link_cnt})
-            links.append([link_cnt, nodes[3 + (2 + args.mask_cnt) * i]["id"], 0, nodes[4 + (2 + args.mask_cnt) * i]["id"], 0])
+    ## Image Loader -> Image Scaler
+    for i in range(args.image_cnt):
+        nodes[3 + (2 + args.mask_cnt) * i]["outputs"][0]["links"].append(link_cnt)
+        nodes[4 + (2 + args.mask_cnt) * i]["inputs"][0].update({"link": link_cnt})
+        links.append([link_cnt, nodes[3 + (2 + args.mask_cnt) * i]["id"], 0, nodes[4 + (2 + args.mask_cnt) * i]["id"], 0])
+        link_cnt += 1
+
+        for j in range(args.mask_cnt):
+            ## Checkpoint -> Mask-KSampler
+            nodes[0]["outputs"][0]["links"].append(link_cnt)
+            nodes[5 + (2 + args.mask_cnt) * i + j]["inputs"][2].update({"link": link_cnt})
+            links.append([link_cnt, nodes[0]["id"], 0, nodes[5 + (2 + args.mask_cnt) * i + j]["id"], 2])
+            link_cnt += 1
+            ## Checkpoint -> Mask-VAE Encoder
+            nodes[0]["outputs"][2]["links"].append(link_cnt)
+            nodes[5 + (2 + args.mask_cnt) * i + j]["inputs"][1].update({"link": link_cnt})
+            links.append([link_cnt, nodes[0]["id"], 2, nodes[5 + (2 + args.mask_cnt) * i + j]["id"], 1])
+            link_cnt += 1
+            ## Checkpoint -> Mask-VAE Decoder
+            nodes[0]["outputs"][2]["links"].append(link_cnt)
+            nodes[5 + (2 + args.mask_cnt) * i + j]["inputs"][5].update({"link": link_cnt})
+            links.append([link_cnt, nodes[0]["id"], 2, nodes[5 + (2 + args.mask_cnt) * i + j]["id"], 5])
             link_cnt += 1
 
-            for j in range(args.mask_cnt):
-                ## Checkpoint -> Mask-KSampler
-                nodes[0]["outputs"][0]["links"].append(link_cnt)
-                nodes[5 + (2 + args.mask_cnt) * i + j]["inputs"][2].update({"link": link_cnt})
-                links.append([link_cnt, nodes[0]["id"], 0, nodes[5 + (2 + args.mask_cnt) * i + j]["id"], 2])
-                link_cnt += 1
-                ## Checkpoint -> Mask-VAE Encoder
-                nodes[0]["outputs"][2]["links"].append(link_cnt)
-                nodes[5 + (2 + args.mask_cnt) * i + j]["inputs"][1].update({"link": link_cnt})
-                links.append([link_cnt, nodes[0]["id"], 2, nodes[5 + (2 + args.mask_cnt) * i + j]["id"], 1])
-                link_cnt += 1
-                ## Checkpoint -> Mask-VAE Decoder
-                nodes[0]["outputs"][2]["links"].append(link_cnt)
-                nodes[5 + (2 + args.mask_cnt) * i + j]["inputs"][5].update({"link": link_cnt})
-                links.append([link_cnt, nodes[0]["id"], 2, nodes[5 + (2 + args.mask_cnt) * i + j]["id"], 5])
-                link_cnt += 1
-
-                ## CLIP Encoder -> Mask-KSampler
-                nodes[1]["outputs"][0]["links"].append(link_cnt)
-                nodes[5 + (2 + args.mask_cnt) * i + j]["inputs"][3].update({"link": link_cnt})
-                links.append([link_cnt, nodes[1]["id"], 0, nodes[5 + (2 + args.mask_cnt) * i + j]["id"], 3])
-                link_cnt += 1
-                nodes[2]["outputs"][0]["links"].append(link_cnt)
-                nodes[5 + (2 + args.mask_cnt) * i + j]["inputs"][4].update({"link": link_cnt})
-                links.append([link_cnt, nodes[2]["id"], 0, nodes[5 + (2 + args.mask_cnt) * i + j]["id"], 4])
-                link_cnt += 1
-
-                ## Image Scaler -> Mask-VAE Encoder
-                nodes[4 + (2 + args.mask_cnt) * i]["outputs"][0]["links"].append(link_cnt)
-                nodes[5 + (2 + args.mask_cnt) * i + j]["inputs"][0].update({"link": link_cnt})
-                links.append([link_cnt, nodes[4 + (2 + args.mask_cnt) * i]["id"], 0, nodes[5 + (2 + args.mask_cnt) * i + j]["id"], 0])
-                link_cnt += 1
-
-    else:
-        ## Image Loader -> Image Scaler
-        for i in range(args.image_cnt):
-            nodes[3 + (2 + 5 * args.mask_cnt) * i]["outputs"][0]["links"].append(link_cnt)
-            nodes[4 + (2 + 5 * args.mask_cnt) * i]["inputs"][0].update({"link": link_cnt})
-            links.append([link_cnt, nodes[3 + (2 + 5 * args.mask_cnt) * i]["id"], 0, nodes[4 + (2 + 5 * args.mask_cnt) * i]["id"], 0])
+            ## CLIP Encoder -> Mask-KSampler
+            nodes[1]["outputs"][0]["links"].append(link_cnt)
+            nodes[5 + (2 + args.mask_cnt) * i + j]["inputs"][3].update({"link": link_cnt})
+            links.append([link_cnt, nodes[1]["id"], 0, nodes[5 + (2 + args.mask_cnt) * i + j]["id"], 3])
+            link_cnt += 1
+            nodes[2]["outputs"][0]["links"].append(link_cnt)
+            nodes[5 + (2 + args.mask_cnt) * i + j]["inputs"][4].update({"link": link_cnt})
+            links.append([link_cnt, nodes[2]["id"], 0, nodes[5 + (2 + args.mask_cnt) * i + j]["id"], 4])
             link_cnt += 1
 
-            for j in range(args.mask_cnt):
-                ## Checkpoint -> VAE Encoder
-                nodes[0]["outputs"][2]["links"].append(link_cnt)
-                nodes[5 + (2 + 5 * args.mask_cnt) * i + 5 * j]["inputs"][1].update({"link": link_cnt})
-                links.append([link_cnt, nodes[0]["id"], 2, nodes[5 + (2 + 5 * args.mask_cnt) * i + 5 * j]["id"], 1])
-                link_cnt += 1
-
-                ## Image Scaler -> VAE Encoder
-                nodes[4 + (2 + 5 * args.mask_cnt) * i]["outputs"][0]["links"].append(link_cnt)
-                nodes[5 + (2 + 5 * args.mask_cnt) * i + 5 * j]["inputs"][0].update({"link": link_cnt})
-                links.append([link_cnt, nodes[4 + (2 + 5 * args.mask_cnt) * i]["id"], 0, nodes[5 + (2 + 5 * args.mask_cnt) * i + 5 * j]["id"], 0])
-                link_cnt += 1
-
-                ## Image Mask Loader -> VAE Encoder
-                nodes[6 + (2 + 5 * args.mask_cnt) * i + 5 * j]["outputs"][0]["links"].append(link_cnt)
-                nodes[5 + (2 + 5 * args.mask_cnt) * i + 5 * j]["inputs"][2].update({"link": link_cnt})
-                links.append([link_cnt, nodes[6 + (2 + 5 * args.mask_cnt) * i + 5 * j]["id"], 0, nodes[5 + (2 + 5 * args.mask_cnt) * i + 5 * j]["id"], 2])
-                link_cnt += 1
-
-                ## Checkpoint -> KSampler
-                nodes[0]["outputs"][0]["links"].append(link_cnt)
-                nodes[7 + (2 + 5 * args.mask_cnt) * i + 5 * j]["inputs"][0].update({"link": link_cnt})
-                links.append([link_cnt, nodes[0]["id"], 0, nodes[7 + (2 + 5 * args.mask_cnt) * i + 5 * j]["id"], 0])
-                link_cnt += 1
-
-                ## CLIP Encoder -> KSampler
-                nodes[1]["outputs"][0]["links"].append(link_cnt)
-                nodes[7 + (2 + 5 * args.mask_cnt) * i + 5 * j]["inputs"][1].update({"link": link_cnt})
-                links.append([link_cnt, nodes[1]["id"], 0, nodes[7 + (2 + 5 * args.mask_cnt) * i + 5 * j]["id"], 1])
-                link_cnt += 1
-                nodes[2]["outputs"][0]["links"].append(link_cnt)
-                nodes[7 + (2 + 5 * args.mask_cnt) * i + 5 * j]["inputs"][2].update({"link": link_cnt})
-                links.append([link_cnt, nodes[2]["id"], 0, nodes[7 + (2 + 5 * args.mask_cnt) * i + 5 * j]["id"], 2])
-                link_cnt += 1
-
-                ## VAE Encoder -> KSampler
-                nodes[5 + (2 + 5 * args.mask_cnt) * i + 5 * j]["outputs"][0]["links"].append(link_cnt)
-                nodes[7 + (2 + 5 * args.mask_cnt) * i + 5 * j]["inputs"][3].update({"link": link_cnt})
-                links.append([link_cnt, nodes[5 + (2 + 5 * args.mask_cnt) * i + 5 * j]["id"], 0, nodes[7 + (2 + 5 * args.mask_cnt) * i + 5 * j]["id"], 3])
-                link_cnt += 1
-
-                ## Checkpoint -> VAE Decoder
-                nodes[0]["outputs"][0]["links"].append(link_cnt)
-                nodes[8 + (2 + 5 * args.mask_cnt) * i + 5 * j]["inputs"][1].update({"link": link_cnt})
-                links.append([link_cnt, nodes[0]["id"], 2, nodes[9 + (2 + 5 * args.mask_cnt) * i + 5 * j]["id"], 1])
-                link_cnt += 1
-
-                ## KSampler -> VAE Decoder
-                nodes[7 + (2 + 5 * args.mask_cnt) * i + 5 * j]["outputs"][0]["links"].append(link_cnt)
-                nodes[8 + (2 + 5 * args.mask_cnt) * i + 5 * j]["inputs"][0].update({"link": link_cnt})
-                links.append([link_cnt, nodes[7 + (2 + 5 * args.mask_cnt) * i + 5 * j]["id"], 0, nodes[8 + (2 + 5 * args.mask_cnt) * i + 5 * j]["id"], 0])
-                link_cnt += 1
-
-                ## VAE Decoder -> Image Saver
-                nodes[8 + (2 + 5 * args.mask_cnt) * i + 5 * j]["outputs"][0]["links"].append(link_cnt)
-                nodes[9 + (2 + 5 * args.mask_cnt) * i + 5 * j]["inputs"][0].update({"link": link_cnt})
-                links.append([link_cnt, nodes[8 + (2 + 5 * args.mask_cnt) * i + 5 * j]["id"], 0, nodes[9 + (2 + 5 * args.mask_cnt) * i + 5 * j]["id"], 0])
-                link_cnt += 1
+            ## Image Scaler -> Mask-VAE Encoder
+            nodes[4 + (2 + args.mask_cnt) * i]["outputs"][0]["links"].append(link_cnt)
+            nodes[5 + (2 + args.mask_cnt) * i + j]["inputs"][0].update({"link": link_cnt})
+            links.append([link_cnt, nodes[4 + (2 + args.mask_cnt) * i]["id"], 0, nodes[5 + (2 + args.mask_cnt) * i + j]["id"], 0])
+            link_cnt += 1
 
     # dump the workflow
     workflow.update({"nodes": nodes})
     workflow.update({"links": links})
-    if args.group_nodes:
-        workflow.update({"extra": {"groupNodes": GROUP_NODES}})
+    workflow.update({"extra": {"groupNodes": GROUP_NODES}})
     json.dump(workflow, open("workflow.json", "w"), indent=4)
 
 
