e2529eccb5e8f1e8d8b5fa00cebe2751016a6729
manip_ego <52987267+manipEgo@users.noreply.github.com>
2024-08-01 17:22:09 +0800

    chore: add image scaling procedure

--- src/workflow_generation_grid.py
+++ src/workflow_generation_grid.py
@@ -16,6 +16,18 @@ def parse_args() -> argparse.Namespace:
         default=8,
         help="Number of grid on one side",
     )
+    parser.add_argument(
+        "--width",
+        type=int,
+        default=512,
+        help="Image width",
+    )
+    parser.add_argument(
+        "--upscale_method",
+        type=str,
+        default="nearest-exact",
+        help="Upscale method",
+    )
 
     parser.add_argument(
         "--steps",
@@ -104,10 +116,24 @@ def main(args: argparse.Namespace):
         },
     )
     id_cnt += 1
+    # Image Scaler: 4
+    nodes.append(
+        {
+            "id": id_cnt,
+            "type": "ImageScale",
+            "pos": [400, 775],
+            "inputs": [{"name": "image", "type": "IMAGE", "link": None}],
+            "outputs": [
+                {"name": "IMAGE", "type": "IMAGE", "links": [], "slot_index": 0}
+            ],
+            "widgets_values": [args.upscale_method, args.width, args.width, "center"],
+        }
+    )
+    id_cnt += 1
 
     for i in range(args.grid_side):
         for j in range(args.grid_side):
-            # Filename String Function: 4 + i * grid_side * 2 + j * 2
+            # Filename String Function: 5 + i * grid_side * 2 + j * 2
             nodes.append(
                 {
                     "id": id_cnt,
@@ -125,7 +151,7 @@ def main(args: argparse.Namespace):
                 }
             )
             id_cnt += 1
-            # Masked In-paint: 5 + i * grid_side * 2 + j * 2
+            # Masked In-paint: 6 + i * grid_side * 2 + j * 2
             nodes.append(
                 {
                     "id": id_cnt,
@@ -178,45 +204,50 @@ def main(args: argparse.Namespace):
     links.append([link_cnt, nodes[0]["id"], 1, nodes[2]["id"], 0])
     link_cnt += 1
 
+    ## Batch Image Loader -> Image Scaler
+    nodes[3]["outputs"][0]["links"].append(link_cnt)
+    nodes[4]["inputs"][0].update({"link": link_cnt})
+    links.append([link_cnt, nodes[3]["id"], 0, nodes[4]["id"], 0])
+    link_cnt += 1
     for i in range(args.grid_side):
         for j in range(args.grid_side):
             ## String Function -> Masked In-paint
-            nodes[4 + i * args.grid_side * 2 + j * 2]["outputs"][0]["links"].append(link_cnt)
-            nodes[5 + i * args.grid_side * 2 + j * 2]["inputs"][6].update({"link": link_cnt})
-            links.append([link_cnt, nodes[4 + i * args.grid_side * 2 + j * 2]["id"], 0, nodes[5 + i * args.grid_side * 2 + j * 2]["id"], 6])
+            nodes[5 + i * args.grid_side * 2 + j * 2]["outputs"][0]["links"].append(link_cnt)
+            nodes[6 + i * args.grid_side * 2 + j * 2]["inputs"][6].update({"link": link_cnt})
+            links.append([link_cnt, nodes[5 + i * args.grid_side * 2 + j * 2]["id"], 0, nodes[6 + i * args.grid_side * 2 + j * 2]["id"], 6])
             link_cnt += 1
             ## Checkpoint Model -> Masked In-paint
             nodes[0]["outputs"][0]["links"].append(link_cnt)
-            nodes[5 + i * args.grid_side * 2 + j * 2]["inputs"][2].update({"link": link_cnt})
-            links.append([link_cnt, nodes[0]["id"], 0, nodes[5 + i * args.grid_side * 2 + j * 2]["id"], 2])
+            nodes[6 + i * args.grid_side * 2 + j * 2]["inputs"][2].update({"link": link_cnt})
+            links.append([link_cnt, nodes[0]["id"], 0, nodes[6 + i * args.grid_side * 2 + j * 2]["id"], 2])
             link_cnt += 1
             ## Checkpoint VAE -> Masked In-paint
             nodes[0]["outputs"][2]["links"].append(link_cnt)
-            nodes[5 + i * args.grid_side * 2 + j * 2]["inputs"][1].update({"link": link_cnt})
-            links.append([link_cnt, nodes[0]["id"], 2, nodes[5 + i * args.grid_side * 2 + j * 2]["id"], 1])
+            nodes[6 + i * args.grid_side * 2 + j * 2]["inputs"][1].update({"link": link_cnt})
+            links.append([link_cnt, nodes[0]["id"], 2, nodes[6 + i * args.grid_side * 2 + j * 2]["id"], 1])
             link_cnt += 1
             nodes[0]["outputs"][2]["links"].append(link_cnt)
-            nodes[5 + i * args.grid_side * 2 + j * 2]["inputs"][5].update({"link": link_cnt})
-            links.append([link_cnt, nodes[0]["id"], 2, nodes[5 + i * args.grid_side * 2 + j * 2]["id"], 5])
+            nodes[6 + i * args.grid_side * 2 + j * 2]["inputs"][5].update({"link": link_cnt})
+            links.append([link_cnt, nodes[0]["id"], 2, nodes[6 + i * args.grid_side * 2 + j * 2]["id"], 5])
             link_cnt += 1
             ## CLIP -> Masked In-paint
             nodes[1]["outputs"][0]["links"].append(link_cnt)
-            nodes[5 + i * args.grid_side * 2 + j * 2]["inputs"][3].update({"link": link_cnt})
-            links.append([link_cnt, nodes[1]["id"], 0, nodes[5 + i * args.grid_side * 2 + j * 2]["id"], 3])
+            nodes[6 + i * args.grid_side * 2 + j * 2]["inputs"][3].update({"link": link_cnt})
+            links.append([link_cnt, nodes[1]["id"], 0, nodes[6 + i * args.grid_side * 2 + j * 2]["id"], 3])
             link_cnt += 1
             nodes[2]["outputs"][0]["links"].append(link_cnt)
-            nodes[5 + i * args.grid_side * 2 + j * 2]["inputs"][4].update({"link": link_cnt})
-            links.append([link_cnt, nodes[2]["id"], 0, nodes[5 + i * args.grid_side * 2 + j * 2]["id"], 4])
+            nodes[6 + i * args.grid_side * 2 + j * 2]["inputs"][4].update({"link": link_cnt})
+            links.append([link_cnt, nodes[2]["id"], 0, nodes[6 + i * args.grid_side * 2 + j * 2]["id"], 4])
             link_cnt += 1
-            ## Batch Image Loader -> Masked In-paint
-            nodes[3]["outputs"][0]["links"].append(link_cnt)
-            nodes[5 + i * args.grid_side * 2 + j * 2]["inputs"][0].update({"link": link_cnt})
-            links.append([link_cnt, nodes[3]["id"], 0, nodes[5 + i * args.grid_side * 2 + j * 2]["id"], 0])
+            ## Image Scaler -> Masked In-paint
+            nodes[4]["outputs"][0]["links"].append(link_cnt)
+            nodes[6 + i * args.grid_side * 2 + j * 2]["inputs"][0].update({"link": link_cnt})
+            links.append([link_cnt, nodes[4]["id"], 0, nodes[6 + i * args.grid_side * 2 + j * 2]["id"], 0])
             link_cnt += 1
             ## Batch Image Loader -> String Function
             nodes[3]["outputs"][1]["links"].append(link_cnt)
-            nodes[4 + i * args.grid_side * 2 + j * 2]["inputs"][0].update({"link": link_cnt})
-            links.append([link_cnt, nodes[3]["id"], 1, nodes[4 + i * args.grid_side * 2 + j * 2]["id"], 0])
+            nodes[5 + i * args.grid_side * 2 + j * 2]["inputs"][0].update({"link": link_cnt})
+            links.append([link_cnt, nodes[3]["id"], 1, nodes[5 + i * args.grid_side * 2 + j * 2]["id"], 0])
             link_cnt += 1
 
 
